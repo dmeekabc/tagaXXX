73def39d0e72d98b1a4240585589324a457f7587
Darrin Meek <darrinmeek@aolcom>
2016-03-10 09:34:53 -0500

    2016 updates

--- wrapRemoteWiresharkCapture.sh
+++ wrapRemoteWiresharkCapture.sh
@@ -8,6 +8,22 @@ source $TAGA_DIR/config
 
 echo $0 : $MYIP :  executing at `date`
 
+REMOTE_NODE=$FIXED_ONE_LIST
+REMOTE_NODE_ITFC=$FIXED_ONE_LIST_ITFC
+echo $0 : $MYIP remote to $REMOTE_NODE and $REMOTE_NODE_ITFC:  executing at `date`
+
+# issue confirmation prompt
+./confirm.sh
+
+let response=$?
+if [ $response -eq 1 ]; then
+  echo; echo Rebooting $FIXED_ONE_LIST ....; echo
+else
+  echo; echo Reboot $FIXED_ONE_LIST Command Not Confirmed, Exiting without action...; echo
+  exit
+fi
+
+
 ./remoteWiresharkCapture.sh 1
 sleep 5
 ./remoteWiresharkCapture.sh 2 &
