03ccb6c922ec3fe5fbb373ea7fde44875084154d
Darrin Meek <darrinmeek@aolcom>
2016-03-10 18:23:00 -0500

    2016 updates

--- adminstats.sh
+++ adminstats.sh
@@ -6,6 +6,16 @@
 TAGA_DIR=~/scripts/taga
 source $TAGA_DIR/config
 
-
-echo IP Usage: $INTERFACE: `ifconfig $INTERFACE | grep RX | grep bytes`
+if [ $# -eq 1 ]; then
+   if [ $1 == "RXonly" ]; then
+      ifconfig $INTERFACE | grep "RX bytes" | cut -d: -f 2 | cut -d\( -f 1
+   elif [ $1 == "TXonly" ]; then
+      ifconfig $INTERFACE | grep "TX bytes" | cut -d: -f 3 | cut -d\( -f 1
+   else
+      echo IP Usage: $INTERFACE: `ifconfig $INTERFACE | grep RX | grep bytes`
+   fi
+else
+  echo 4
+  echo IP Usage: $INTERFACE: `ifconfig $INTERFACE | grep RX | grep bytes`
+fi
 
