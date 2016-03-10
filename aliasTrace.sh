9d381566d1ce64587c0dbb9bb3572d28717be96e
Darrin Meek <darrinmeek@aolcom>
2016-03-10 10:51:03 -0500

    2016 updates

---
+++ aliasTrace.sh
@@ -0,0 +1,43 @@
+#####################################################
+# Copyright 2016 IBOA Corp
+# All Rights Reserved
+#####################################################
+
+TAGA_DIR=~/scripts/taga
+source $TAGA_DIR/config
+
+echo $0 : $MYIP :  executing at `date`; echo
+
+source ~/.bashrc
+
+# note, prior to running this script, # run the following: 
+#
+#    alias > $TAGA_DIR/aliasExamples.txt
+#
+source ./aliasExamples.txt
+
+aliasNext=`alias $1`
+RET=$?
+echo
+echo $aliasNext
+echo
+
+aliasNext=`echo $aliasNext | cut -d\' -f 2`
+RET=$?
+
+while [ $RET -eq 0 ] 
+do
+   echo $aliasNext
+   aliasNext=`alias $aliasNext` 2>/dev/null
+   RET=$?
+   if [ $RET -eq 0 ]; then
+      echo
+      echo $aliasNext
+      echo
+      #echo RET:$RET
+      aliasNext=`echo $aliasNext | cut -d\' -f 2`
+   else
+      echo; echo End of the line
+   fi
+done
+
