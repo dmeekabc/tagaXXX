55c06115e5b2c0263f8bd844631f5ff1400afccd
Darrin Meek <darrinmeek@aolcom>
2016-03-09 23:52:19 -0500

    2016 updates

---
+++ a.sh
@@ -0,0 +1,10 @@
+#####################################################
+# Copyright 2016 IBOA Corp
+# All Rights Reserved
+#####################################################
+
+TAGA_DIR=~/scripts/taga
+source $TAGA_DIR/config
+
+echo $0 : $MYIP :  executing at `date`
+
