248b100f3cc4539405b242235d3acaea89984709
Darrin Meek <darrinmeek@aolcom>
2016-03-11 00:21:58 -0500

    2016 updates

--- runLoop.sh
+++ runLoop.sh
@@ -526,7 +526,23 @@ do
 
    wordlen=`echo $DELTA_TX_STATS | awk '{print length($0)}'`
    echo DeltaTxStatus wordlen: $wordlen
-   if [ $wordlen -eq 7 ]; then
+
+
+
+
+
+
+
+
+
+
+
+   if [ $wordlen -eq 8 ]; then
+      let MBytes=$DELTA_TX_STATS*10 # multiply by 10 to get fraction
+      let MBytes=$MBytes/1000000
+      megabytePrint=`echo $MBytes | cut -c1-2`.`echo $MBytes | cut -c3`
+      echo DELTA_TX_STATS: $DELTA_TX_STATS \($megabytePrint MB\)
+   elif [ $wordlen -eq 7 ]; then
       let MBytes=$DELTA_TX_STATS*10 # multiply by 10 to get fraction
       let MBytes=$MBytes/1000000
       megabytePrint=`echo $MBytes | cut -c1`.`echo $MBytes | cut -c2`
@@ -535,26 +551,70 @@ do
       let KBytes=$DELTA_TX_STATS*10 # multiply by 10 to get fraction
       let KBytes=$KBytes/1000
       kilobytePrint=`echo $KBytes | cut -c1-3`.`echo $KBytes | cut -c4`
-      #kilobytePrint=`echo $KBytes | cut -c1`.`echo $KBytes | cut -c2`
+      echo DELTA_TX_STATS: $DELTA_TX_STATS \($kilobytePrint KB\)
+   elif [ $wordlen -eq 5 ]; then
+      let KBytes=$DELTA_TX_STATS*10 # multiply by 10 to get fraction
+      let KBytes=$KBytes/1000
+      kilobytePrint=`echo $KBytes | cut -c1-2`.`echo $KBytes | cut -c3-4`
+      echo DELTA_TX_STATS: $DELTA_TX_STATS \($kilobytePrint KB\)
+   elif [ $wordlen -eq 4 ]; then
+      let KBytes=$DELTA_TX_STATS*10 # multiply by 10 to get fraction
+      let KBytes=$KBytes/1000
+      kilobytePrint=`echo $KBytes | cut -c1`.`echo $KBytes | cut -c2-4`
       echo DELTA_TX_STATS: $DELTA_TX_STATS \($kilobytePrint KB\)
    fi
+
+
+
+
+
+
 
    echo DELTA_TX_STATS: $DELTA_TX_STATS
    echo DELTA_RX_STATS: $DELTA_RX_STATS
 
    wordlen=`echo $DELTA_RX_STATS | awk '{print length($0)}'`
    echo DeltaRxStatus wordlen: $wordlen
-   if [ $wordlen -eq 7 ]; then
+
+   if [ $wordlen -eq 8 ]; then
       let MBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
       let MBytes=$MBytes/1000000
-      megabytePrint=`echo $MBytes | cut -c1`.`echo $KBytes | cut -c2`
+      megabytePrint=`echo $MBytes | cut -c1-2`.`echo $MBytes | cut -c3`
+      echo DELTA_RX_STATS: $DELTA_RX_STATS \($megabytePrint MB\)
+   elif [ $wordlen -eq 7 ]; then
+      let MBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
+      let MBytes=$MBytes/1000000
+      megabytePrint=`echo $MBytes | cut -c1`.`echo $MBytes | cut -c2`
       echo DELTA_RX_STATS: $DELTA_RX_STATS \($megabytePrint MB\)
    elif [ $wordlen -eq 6 ]; then
       let KBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
       let KBytes=$KBytes/1000
       kilobytePrint=`echo $KBytes | cut -c1-3`.`echo $KBytes | cut -c4`
       echo DELTA_RX_STATS: $DELTA_RX_STATS \($kilobytePrint KB\)
+   elif [ $wordlen -eq 5 ]; then
+      let KBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
+      let KBytes=$KBytes/1000
+      kilobytePrint=`echo $KBytes | cut -c1-2`.`echo $KBytes | cut -c3-4`
+      echo DELTA_RX_STATS: $DELTA_RX_STATS \($kilobytePrint KB\)
+   elif [ $wordlen -eq 4 ]; then
+      let KBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
+      let KBytes=$KBytes/1000
+      kilobytePrint=`echo $KBytes | cut -c1`.`echo $KBytes | cut -c2-4`
+      echo DELTA_RX_STATS: $DELTA_RX_STATS \($kilobytePrint KB\)
    fi
+
+
+#   if [ $wordlen -eq 7 ]; then
+#      let MBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
+#      let MBytes=$MBytes/1000000
+#      megabytePrint=`echo $MBytes | cut -c1`.`echo $KBytes | cut -c2`
+#      echo DELTA_RX_STATS: $DELTA_RX_STATS \($megabytePrint MB\)
+#   elif [ $wordlen -eq 6 ]; then
+#      let KBytes=$DELTA_RX_STATS*10 # multiply by 10 to get fraction
+#      let KBytes=$KBytes/1000
+#      kilobytePrint=`echo $KBytes | cut -c1-3`.`echo $KBytes | cut -c4`
+#      echo DELTA_RX_STATS: $DELTA_RX_STATS \($kilobytePrint KB\)
+#   fi
 
    echo DELTA_TX_STATS: $DELTA_TX_STATS
    echo DELTA_RX_STATS: $DELTA_RX_STATS
