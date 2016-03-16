cae72349a6d483ccd97850f2458e4858495faf18
Darrin Meek <darrinmeek@aolcom>
2016-03-16 11:13:58 -0400

    March 2016 updates

--- stopAll.sh
+++ stopAll.sh
@@ -8,8 +8,14 @@ source $TAGA_DIR/config
 
 for target in $targetList
 do
+   if [ $target == $MYIP ]; then
+      echo skipping self for now...
+      continue
+   fi
    echo processing, cleaning $target
    ssh -l $MYLOGIN_ID $target $TAGA_DIR/stop.sh  <$TAGA_DIR/passwd.txt
 done
 
+# do myself last
+ssh -l $MYLOGIN_ID $MYIP $TAGA_DIR/stop.sh  <$TAGA_DIR/passwd.txt
 
